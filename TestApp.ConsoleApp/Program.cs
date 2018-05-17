5d8d1f40337636f0ddc87854cd18002590be7b1c
GZA <gzapas@email.com>
2018-05-17 14:10:21 +0300

    Initial Commit

---
+++ TestApp.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestApp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
