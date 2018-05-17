51173f9b8e83a5f12afeb819e7e553295902024c
GZA <gzapas@email.com>
2018-05-17 15:12:49 +0300

    Change

--- TestApp.ConsoleApp/Program.cs
+++ TestApp.ConsoleApp/Program.cs
@@ -7,6 +7,7 @@ namespace TestApp.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("test commit");
         }
     }
 }
